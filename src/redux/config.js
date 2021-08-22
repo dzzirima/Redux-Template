0664f0337ed22927e60d4eed77e9acf331b7b95e
dzzirima@gmail.com <dzzirima@gmail.com>
2021-08-22 11:25:15 +0200

    done with the setup

---
+++ src/redux/config.js
@@ -0,0 +1,22 @@
+// in this file  we will create a store that is going to be passed to the provider in the index.js
+
+import { createStore,combineReducers } from "redux";
+import counterReducer  from "./ducks/counter";
+
+
+//2.import all the reducers fro the ducks
+const reducers = combineReducers({
+    counter:counterReducer,
+    // add all the reducers here  even if you had a reducer which deals   with user
+})
+
+//3.pass the reducers to the store
+//1.then 3 creating a store  and pass the reducer
+const store = createStore(reducers)
+
+
+
+
+
+
+export default store
---
+++ src/redux/ducks/counter.js
@@ -0,0 +1,31 @@
+// create all the files actions and reducers here and export an unnamed function which uses the switch
+
+
+//1. initialise the state
+let initialState = {
+    count:0
+}
+
+
+//2.create your name reducers
+const INCREASE = 'increase'
+
+//3.create an action  which maps directly to the const above
+export const increase = () =>({
+    type:INCREASE
+}
+) 
+
+//4.create the reducer
+export default (state=initialState,action)=>{ // initialising the sate from state
+
+    switch (action.type) {
+        case INCREASE:
+            // do the actual changing of the values heree
+            return {...state , count:state.count+1}
+        default:
+            // remember to return state for the default 
+            return state
+    }
+
+}
