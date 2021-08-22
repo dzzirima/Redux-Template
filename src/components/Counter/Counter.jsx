74a4738805571e3135e3a15038fbf02101663eec
dzzirima@gmail.com <dzzirima@gmail.com>
2021-08-22 10:44:18 +0200

    initial commit

---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Counter = () => {
+    return (
+        <div>
+            <h2>This is my Counter</h2>
+
+            <button> Add</button>
+            <br/>
+            <button>Decrease</button>
+        </div>
+    )
+}
